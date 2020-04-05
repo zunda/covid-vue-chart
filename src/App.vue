2f3dfc696a99ddf8df39e651c7a705f5e0f98077
zunda <zundan@gmail.com>
2020-04-05 13:19:05 -1000

    Plot random line chart
    following https://vue-chartjs.org/guide/#example
    
    1. `yarn add vue-chartjs chart.js`
    2. Add example files `RandomChart.vue` and `LineChart.js` to `src/components`
    3. Edit `src/App.vue`

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RandomChart/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RandomChart from './components/RandomChart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RandomChart
   }
 }
 </script>
