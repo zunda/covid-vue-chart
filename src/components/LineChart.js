d0c6f87dd7a6f91bd326ad1319eafeeca74713b7
zunda <zundan@gmail.com>
2020-04-05 14:29:15 -1000

    Use mixins
    Now following https://medium.com/js-dojo/visualizing-time-series-data-in-vue-js-6a917ea5d869

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,7 +1,8 @@
-import { Line } from 'vue-chartjs'
+import { Line, mixins } from 'vue-chartjs'
 
 export default {
   extends: Line,
+  mixins: [mixins.reactiveProp],
   props: {
     chartData: {
       type: Object,
