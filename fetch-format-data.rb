3971813a6a2b9528b582eda6b7187be0772a98b9
zunda <zundan@gmail.com>
2020-05-13 09:56:51 -1000

    Update column names for Japan data
    Shoutout to https://github.com/kaz-ogiwara/covid19 :hearts:

--- fetch-format-data.rb
+++ fetch-format-data.rb
@@ -31,56 +31,6 @@ TIMESERIES='src/assets/timeSeries.json'
 NEWCASES='src/assets/newCases.json'
 FOOTNOTE='src/assets/footnote.json'
 
-Prefectures = {
-  "北海道" => "Hokkaido",
-  "青森県" => "Aomori",
-  "岩手県" => "Iwate",
-  "宮城県" => "Miyagi",
-  "秋田県" => "Akita",
-  "山形県" => "Yamagata",
-  "福島県" => "Fukushima",
-  "茨城県" => "Ibaraki",
-  "栃木県" => "Tochigi",
-  "群馬県" => "Gunma",
-  "埼玉県" => "Saitama",
-  "千葉県" => "Chiba",
-  "東京都" => "Tokyo",
-  "神奈川県" => "Kanagawa",
-  "新潟県" => "Niigata",
-  "富山県" => "Toyama",
-  "石川県" => "Ishikawa",
-  "福井県" => "Fukui",
-  "山梨県" => "Yamanashi",
-  "長野県" => "Nagano",
-  "岐阜県" => "Gifu",
-  "静岡県" => "Shizuoka",
-  "愛知県" => "Aichi",
-  "三重県" => "Mie",
-  "滋賀県" => "Shiga",
-  "京都府" => "Kyoto",
-  "大阪府" => "Osaka",
-  "兵庫県" => "Hyogo",
-  "奈良県" => "Nara",
-  "和歌山県" => "Wakayama",
-  "鳥取県" => "Tottori",
-  "島根県" => "Shimane",
-  "岡山県" => "Okayama",
-  "広島県" => "Hiroshima",
-  "山口県" => "Yamaguchi",
-  "徳島県" => "Tokushima",
-  "香川県" => "Kagawa",
-  "愛媛県" => "Ehime",
-  "高知県" => "Kochi",
-  "福岡県" => "Fukuoka",
-  "佐賀県" => "Saga",
-  "長崎県" => "Nagasaki",
-  "熊本県" => "Kumamoto",
-  "大分県" => "Oita",
-  "宮崎県" => "Miyazaki",
-  "鹿児島県" => "Kagoshima",
-  "沖縄県" => "Okinawa"
-}
-
 def parsedate(str)
 	if str =~ %r[\d+/\d+/\d+]
 		Time.strptime(str + ' +0000', '%m/%d/%y %z')
@@ -145,11 +95,10 @@ _END
 
 $stderr.puts "Fetching and counting data for Japan"
 parse_csv(JAPAN_CONFIRMED) do |data|
-  p = data['都道府県'].strip
-  p = Prefectures[p] || p
+  p = data['prefectureNameE'].strip
   region = ['Japan', p]
-  date = Time.utc(data['年'], data['月'], data['日'])
-  counts[region][date] += Integer(data['患者数（2020年3月28日からは感染者数）'].gsub(/,/, ''))
+  date = Time.utc(data['year'], data['month'], data['date'])
+  counts[region][date] += Integer(data['testedPositive'].gsub(/,/, ''))
 end
 footnote += <<_END
 Data for Japan are from <a href="https://github.com/kaz-ogiwara/covid19">kaz-ogiwara/covid19</a> &copy; TOYO KEIZAI ONLINE.
