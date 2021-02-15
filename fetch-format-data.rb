a27314255ac11816152f1d832b046037e1b2c9a8
zunda <zundan@gmail.com>
2021-02-15 10:58:37 -1000

    Make it possible to load fetch-format-data.rb as a library

--- fetch-format-data.rb
+++ fetch-format-data.rb
@@ -66,135 +66,137 @@ def count_up_jhu(hash, row, region)
   end
 end
 
-timestamps = {refreshed: Time.now.utc}
-footnote =<<_END
+if __FILE__ == $0
+  timestamps = {refreshed: Time.now.utc}
+  footnote =<<_END
 Data were refreshed around #{Time.now.utc.strftime("%Y-%m-%d %H:%M %Z")}.
 _END
-counts = Hash.new{|h, region| h[region] = Hash.new{|j, date| j[date] = 0}}
+  counts = Hash.new{|h, region| h[region] = Hash.new{|j, date| j[date] = 0}}
 
-$stderr.puts "Fetching and counting global data"
-parse_csv(GLOBAL_CONFIRMED) do |data|
-  c = data['Country/Region'].strip
-  s = data['Province/State']&.strip
-  count_up_jhu(counts, data, [c])
-  count_up_jhu(counts, data, [c, s]) if s and not s.empty?
-  count_up_jhu(counts, data, ['World'])
-end
+  $stderr.puts "Fetching and counting global data"
+  parse_csv(GLOBAL_CONFIRMED) do |data|
+    c = data['Country/Region'].strip
+    s = data['Province/State']&.strip
+    count_up_jhu(counts, data, [c])
+    count_up_jhu(counts, data, [c, s]) if s and not s.empty?
+    count_up_jhu(counts, data, ['World'])
+  end
 
-$stderr.puts "Fetching and counting data for US"
-parse_csv(US_CONFIRMED) do |data|
-  c = data['Country_Region'].strip
-  s = data['Province_State'].strip
-  a = data['Admin2']&.strip
-  count_up_jhu(counts, data, [c, s])
-  count_up_jhu(counts, data, [c, s, a]) if a and not a.empty?
-end
-footnote += <<_END
+  $stderr.puts "Fetching and counting data for US"
+  parse_csv(US_CONFIRMED) do |data|
+    c = data['Country_Region'].strip
+    s = data['Province_State'].strip
+    a = data['Admin2']&.strip
+    count_up_jhu(counts, data, [c, s])
+    count_up_jhu(counts, data, [c, s, a]) if a and not a.empty?
+  end
+  footnote += <<_END
 Global and US data are from <a href="https://github.com/CSSEGISandData/COVID-19">CSSEGISandData/COVID-19</a> &copy; 2020 Johns Hopkins University, educational and academic research purposes only.
 _END
 
-$stderr.puts "Fetching and counting data for Japan"
-parse_csv(JAPAN_CONFIRMED) do |data|
-  p = data['prefectureNameE'].strip
-  region = ['Japan', p]
-  date = Time.utc(data['year'], data['month'], data['date'])
-  counts[region][date] += Integer(data['testedPositive'].gsub(/,/, ''))
-end
-footnote += <<_END
+  $stderr.puts "Fetching and counting data for Japan"
+  parse_csv(JAPAN_CONFIRMED) do |data|
+    p = data['prefectureNameE'].strip
+    region = ['Japan', p]
+    date = Time.utc(data['year'], data['month'], data['date'])
+    counts[region][date] += Integer(data['testedPositive'].gsub(/,/, ''))
+  end
+  footnote += <<_END
 Data for Japan are from <a href="https://github.com/kaz-ogiwara/covid19">kaz-ogiwara/covid19</a> &copy; TOYO KEIZAI ONLINE and are available through 2021-01-30.
 _END
 
-$stderr.puts "Fetching and counting data for Tokyo"
-c = Hash.new{0}
-parse_csv(TOKYO_CONFIRMED) do |data|
-  next unless data['公表_年月日']
-  begin
-    date = Time.strptime(data['公表_年月日'] + " UTC", "%Y-%m-%d %z")
-  rescue NoMethodError => err
-    raise err.exception(data.inspect)
+  $stderr.puts "Fetching and counting data for Tokyo"
+  c = Hash.new{0}
+  parse_csv(TOKYO_CONFIRMED) do |data|
+    next unless data['公表_年月日']
+    begin
+      date = Time.strptime(data['公表_年月日'] + " UTC", "%Y-%m-%d %z")
+    rescue NoMethodError => err
+      raise err.exception(data.inspect)
+    end
+    c[date] += 1
   end
-  c[date] += 1
-end
-r = ['Japan', 'Tokyo']
-counts.delete(r)
-n = 0
-c.keys.sort.each do |date|
-  n += c[date]
-  counts[r][date] = n
-end
-footnote += <<_END
+  r = ['Japan', 'Tokyo']
+  counts.delete(r)
+  n = 0
+  c.keys.sort.each do |date|
+    n += c[date]
+    counts[r][date] = n
+  end
+  footnote += <<_END
 Data for Tokyo are from <a href="https://stopcovid19.metro.tokyo.lg.jp/">stopcovid19.metro.tokyo.lg.jp</a> &copy; 2020 Tokyo Metropolitan Government.
 _END
 
-$stderr.puts "Listing regions"
-region_tree = Array.new
-r = counts.keys.sort
-i = 0
-n = r.length
-while i < n
-  c = r[i][0]
-  region_tree << {id: c, label: c}
-  i += 1
-  while i < n and c == r[i][0]
-    cs = "#{r[i][0]}/#{r[i][1]}"
-    region_tree.last[:children] ||= Array.new
-    region_tree.last[:children] << {id: cs, label: r[i][1]}
+  $stderr.puts "Listing regions"
+  region_tree = Array.new
+  r = counts.keys.sort
+  i = 0
+  n = r.length
+  while i < n
+    c = r[i][0]
+    region_tree << {id: c, label: c}
     i += 1
-    while i < n and cs == "#{r[i][0]}/#{r[i][1]}"
-      csa = "#{r[i][0]}/#{r[i][1]}/#{r[i][2]}"
-      region_tree.last[:children].last[:children] ||= Array.new
-      region_tree.last[:children].last[:children] << {id: csa, label: r[i][2]}
+    while i < n and c == r[i][0]
+      cs = "#{r[i][0]}/#{r[i][1]}"
+      region_tree.last[:children] ||= Array.new
+      region_tree.last[:children] << {id: cs, label: r[i][1]}
       i += 1
+      while i < n and cs == "#{r[i][0]}/#{r[i][1]}"
+        csa = "#{r[i][0]}/#{r[i][1]}/#{r[i][2]}"
+        region_tree.last[:children].last[:children] ||= Array.new
+        region_tree.last[:children].last[:children] << {id: csa, label: r[i][2]}
+        i += 1
+      end
     end
   end
-end
-File.open(REGIONS, 'w') do |f|
-  f.print region_tree.to_json
-end
+  File.open(REGIONS, 'w') do |f|
+    f.print region_tree.to_json
+  end
 
-footnote += <<_END
+  footnote += <<_END
 Check licenses and fork me at <a href="https://github.com/zunda/covid-vue-chart">zunda/covid-vue-chart</a>.
 _END
 
-$stderr.puts "Calculating moving average"
-new_cases = Hash.new{|h, region| h[region] = Hash.new{|j, date| j[date] = 0}}
-avg_half_width = AVG_HALF_WIDTH * 24 * 3600
-avg_step = AVG_STEP * 24 * 3600
-counts.each_pair do |region, cumul|
-  dates = cumul.keys.sort
-  p = 0
-  diffs = dates.map{|d| x = cumul[d] - p; p = cumul[d]; [d, x]}.to_h
-  d1 = dates.first - avg_half_width
-  d2 = dates.last - avg_half_width
-  d1.to_i.step(d2.to_i, avg_step).each do |x|
-    d = Time.at(x).utc
-    dmin = d - avg_half_width
-    dmax = d + avg_half_width
-    data = diffs.select{|k, v| dmin <= k && k <= dmax}.values
-    if data.length > 0
-      new_cases[region][d] = data.inject(0.0, :+) / data.length
-    else
-      new_cases[region][d] = 0
+  $stderr.puts "Calculating moving average"
+  new_cases = Hash.new{|h, region| h[region] = Hash.new{|j, date| j[date] = 0}}
+  avg_half_width = AVG_HALF_WIDTH * 24 * 3600
+  avg_step = AVG_STEP * 24 * 3600
+  counts.each_pair do |region, cumul|
+    dates = cumul.keys.sort
+    p = 0
+    diffs = dates.map{|d| x = cumul[d] - p; p = cumul[d]; [d, x]}.to_h
+    d1 = dates.first - avg_half_width
+    d2 = dates.last - avg_half_width
+    d1.to_i.step(d2.to_i, avg_step).each do |x|
+      d = Time.at(x).utc
+      dmin = d - avg_half_width
+      dmax = d + avg_half_width
+      data = diffs.select{|k, v| dmin <= k && k <= dmax}.values
+      if data.length > 0
+        new_cases[region][d] = data.inject(0.0, :+) / data.length
+      else
+        new_cases[region][d] = 0
+      end
     end
   end
-end
 
-$stderr.puts "Formatting data"
-{cumulativeCases: counts, newCases: new_cases}.each_pair do |type, input|
-  input.each_pair do |r, c|
-    ts = c.keys.sort
-    data = ts.map{|d| {x: d.to_i * 1000, y: c[d]}}
+  $stderr.puts "Formatting data"
+  {cumulativeCases: counts, newCases: new_cases}.each_pair do |type, input|
+    input.each_pair do |r, c|
+      ts = c.keys.sort
+      data = ts.map{|d| {x: d.to_i * 1000, y: c[d]}}
 
-    dst = "public/#{type}/#{r.join("/")}.json"
-    FileUtils.mkdir_p(File.dirname(dst))
-    File.open(dst, 'w') do |f|
-      f.print data.to_json
+      dst = "public/#{type}/#{r.join("/")}.json"
+      FileUtils.mkdir_p(File.dirname(dst))
+      File.open(dst, 'w') do |f|
+        f.print data.to_json
+      end
+      ts = Time.at(ts[-1])
+      FileUtils.touch(dst, mtime: ts)
     end
-    ts = Time.at(ts[-1])
-    FileUtils.touch(dst, mtime: ts)
   end
-end
 
-File.open(FOOTNOTE, 'w') do |f|
-  f.print footnote.to_json
+  File.open(FOOTNOTE, 'w') do |f|
+    f.print footnote.to_json
+  end
 end
