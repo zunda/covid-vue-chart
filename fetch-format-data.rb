b38eafaf7ef3d7ad44d02ee0a6af11594520210e
zunda <zundan@gmail.com>
2021-02-15 12:42:05 -1000

    Use data from https://corona.go.jp/dashboard/

--- fetch-format-data.rb
+++ fetch-format-data.rb
@@ -15,9 +15,61 @@ require 'json'
 require 'open-uri'
 require 'fileutils'
 
+JAPAN_PREF_NAMES={
+	'北海道' => 'Hokkaido',
+	'青森県' => 'Aomori',
+	'岩手県' => 'Iwate',
+	'宮城県' => 'Miyagi',
+	'秋田県' => 'Akita',
+	'山形県' => 'Yamagata',
+	'福島県' => 'Fukushima',
+	'茨城県' => 'Ibaraki',
+	'栃木県' => 'Tochigi',
+	'群馬県' => 'Gunma',
+	'埼玉県' => 'Saitama',
+	'千葉県' => 'Chiba',
+	'東京都' => 'Tokyo',
+	'神奈川県' => 'Kanagawa',
+	'新潟県' => 'Niigata',
+	'富山県' => 'Toyama',
+	'石川県' => 'Ishikawa',
+	'福井県' => 'FUkui',
+	'山梨県' => 'Yamanashi',
+	'長野県' => 'Nagano',
+	'岐阜県' => 'Gifu',
+	'静岡県' => 'Shizuoka',
+	'愛知県' => 'Aichi',
+	'三重県' => 'Mie',
+	'滋賀県' => 'Shiga',
+	'京都府' => 'Kyoto',
+	'大阪府' => 'Osaka',
+	'兵庫県' => 'Hyogo',
+	'奈良県' => 'Nara',
+	'和歌山県' => 'Wakayama',
+	'鳥取県' => 'Tottori',
+	'島根県' => 'Shimane',
+	'岡山県' => 'Okayama',
+	'広島県' => 'Hiroshima',
+	'山口県' => 'Yamaguchi',
+	'徳島県' => 'Tokushima',
+	'香川県' => 'Kagawa',
+	'愛媛県' => 'Ehime',
+	'高知県' => 'Kochi',
+	'福岡県' => 'Fukuoka',
+	'佐賀県' => 'Saga',
+	'長崎県' => 'Nagasaki',
+	'熊本県' => 'Kumamoto',
+	'大分県' => 'Oita',
+	'宮崎県' => 'Miyazaki',
+	'鹿児島県' => 'Kagoshima',
+	'沖縄県' => 'Okinawa',
+}
+
 # input
 GLOBAL_CONFIRMED='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 US_CONFIRMED='https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+CORONA_GO_JP='https://opendata.corona.go.jp/api/Covid19JapanAll'
+CORONA_GO_JP_CUTOVER = Time.utc(2021, 1, 1)
 JAPAN_CONFIRMED='https://raw.githubusercontent.com/kaz-ogiwara/covid19/master/data/prefectures.csv'
 TOKYO_CONFIRMED='https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv'
 
@@ -34,6 +86,8 @@ FOOTNOTE='src/assets/footnote.json'
 def parsedate(str)
 	if str =~ %r[\d+/\d+/\d+]
 		Time.strptime(str + ' +0000', '%m/%d/%y %z')
+	elsif str =~ %r[\d{4,4}-\d\d-\d\d]
+		Time.strptime(str + ' +0000', '%Y-%m-%d %z')
 	else
 		nil
 	end
@@ -50,9 +104,7 @@ def parse_data(location, **opts)
     location = File.basename(location)  # to test this script locally
   end
   begin
-    parser.parse(URI.open(location).read.gsub(/\r\n/, "\n"), **parser_opts).each do |data|
-      yield data
-    end
+    yield parser.parse(URI.open(location).read.gsub(/\r\n/, "\n"), **parser_opts)
   rescue OpenURI::HTTPError => error
     raise error.exception("#{location}: #{error.message}")
   end
@@ -82,47 +134,70 @@ _END
   counts = Hash.new{|h, region| h[region] = Hash.new{|j, date| j[date] = 0}}
 
   $stderr.puts "Fetching and counting global data"
-  parse_data(GLOBAL_CONFIRMED) do |data|
-    c = data['Country/Region'].strip
-    s = data['Province/State']&.strip
-    count_up_jhu(counts, data, [c])
-    count_up_jhu(counts, data, [c, s]) if s and not s.empty?
-    count_up_jhu(counts, data, ['World'])
+  parse_data(GLOBAL_CONFIRMED) do |x|
+    x.each do |data|
+      c = data['Country/Region'].strip
+      s = data['Province/State']&.strip
+      count_up_jhu(counts, data, [c])
+      count_up_jhu(counts, data, [c, s]) if s and not s.empty?
+      count_up_jhu(counts, data, ['World'])
+    end
   end
 
   $stderr.puts "Fetching and counting data for US"
-  parse_data(US_CONFIRMED) do |data|
-    c = data['Country_Region'].strip
-    s = data['Province_State'].strip
-    a = data['Admin2']&.strip
-    count_up_jhu(counts, data, [c, s])
-    count_up_jhu(counts, data, [c, s, a]) if a and not a.empty?
+  parse_data(US_CONFIRMED) do |x|
+    x.each do |data|
+      c = data['Country_Region'].strip
+      s = data['Province_State'].strip
+      a = data['Admin2']&.strip
+      count_up_jhu(counts, data, [c, s])
+      count_up_jhu(counts, data, [c, s, a]) if a and not a.empty?
+    end
   end
   footnote += <<_END
 Global and US data are from <a href="https://github.com/CSSEGISandData/COVID-19">CSSEGISandData/COVID-19</a> &copy; 2020 Johns Hopkins University, educational and academic research purposes only.
 _END
 
-  $stderr.puts "Fetching and counting data for Japan"
-  parse_data(JAPAN_CONFIRMED) do |data|
-    p = data['prefectureNameE'].strip
-    region = ['Japan', p]
-    date = Time.utc(data['year'], data['month'], data['date'])
-    counts[region][date] += Integer(data['testedPositive'].gsub(/,/, ''))
+  $stderr.puts "Fetching and counting data for Japan for earlier data"
+  parse_data(JAPAN_CONFIRMED) do |x|
+    x.each do |data|
+      date = Time.utc(data['year'], data['month'], data['date'])
+      if date < CORONA_GO_JP_CUTOVER
+        region = ['Japan', data['prefectureNameE'].strip]
+        counts[region][date] = Integer(data['testedPositive'].gsub(/,/, ''))
+      end
+    end
   end
   footnote += <<_END
-Data for Japan are from <a href="https://github.com/kaz-ogiwara/covid19">kaz-ogiwara/covid19</a> &copy; TOYO KEIZAI ONLINE and are available through 2021-01-30.
+Data for Japan before #{CORONA_GO_JP_CUTOVER.strftime("%Y-%m-%d")} are from <a href="https://github.com/kaz-ogiwara/covid19">kaz-ogiwara/covid19</a> &copy; TOYO KEIZAI ONLINE.
+_END
+
+  $stderr.puts "Fetching and counting data for Japan"
+  parse_data(CORONA_GO_JP, parser: JSON) do |json|
+    json['itemList'].each do |data|
+      date = parsedate(data['date'])
+      if date >= CORONA_GO_JP_CUTOVER
+        region = ['Japan', JAPAN_PREF_NAMES[data['name_jp']]]
+        counts[region][date] = Integer(data['npatients'])
+      end
+    end
+  end
+  footnote += <<_END
+Data for Japan are from and &copy; by <a href="https://corona.go.jp/dashboard/">Office for Novel Coronavirus Disease Control, Cabinet Secretariat, Government of Japan</a>.
 _END
 
   $stderr.puts "Fetching and counting data for Tokyo"
   c = Hash.new{0}
-  parse_data(TOKYO_CONFIRMED) do |data|
-    next unless data['公表_年月日']
-    begin
-      date = Time.strptime(data['公表_年月日'] + " UTC", "%Y-%m-%d %z")
-    rescue NoMethodError => err
-      raise err.exception(data.inspect)
+  parse_data(TOKYO_CONFIRMED) do |x|
+    x.each do |data|
+      next unless data['公表_年月日']
+      begin
+        date = Time.strptime(data['公表_年月日'] + " UTC", "%Y-%m-%d %z")
+      rescue NoMethodError => err
+        raise err.exception(data.inspect)
+      end
+      c[date] += 1
     end
-    c[date] += 1
   end
   r = ['Japan', 'Tokyo']
   counts.delete(r)
