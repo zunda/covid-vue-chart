915cad88e6f305802d9c124738538afb24a04b6f
zunda <zundan@gmail.com>
2021-04-16 14:27:05 -1000

    Fetch some data concurrently
    Overall, time ruby fetch-format-data.rb became
    
    real	1m39.923s
    user	1m27.412s
    sys	0m0.795s
    
    from
    
    real  2m52.650s
    user  2m39.098s
    sys 0m0.725s

--- fetch-format-data.rb
+++ fetch-format-data.rb
@@ -142,71 +142,78 @@ _END
     end
   end
 
-  $stderr.puts "Fetching and counting data for US"
-  parse_data(US_CONFIRMED) do |x|
-    x.each do |data|
-      c = data['Country_Region'].strip
-      s = data['Province_State'].strip
-      a = data['Admin2']&.strip
-      count_up_jhu(counts, data, [c, s])
-      count_up_jhu(counts, data, [c, s, a]) if a and not a.empty?
-    end
-  end
-  footnote += <<_END
-Global and US data are from <a href="https://github.com/CSSEGISandData/COVID-19">CSSEGISandData/COVID-19</a> &copy; 2020 Johns Hopkins University, educational and academic research purposes only.
-_END
-
-  $stderr.puts "Fetching and counting data for Japan for earlier data"
-  parse_data(JAPAN_CONFIRMED) do |x|
-    x.each do |data|
-      date = Time.utc(data['year'], data['month'], data['date'])
-      if date < CORONA_GO_JP_CUTOVER
-        region = ['Japan', data['prefectureNameE'].strip]
-        counts[region][date] = Integer(data['testedPositive'].gsub(/,/, ''))
+  th = []
+  th << Thread.new do
+    $stderr.puts "Fetching and counting data for US"
+    parse_data(US_CONFIRMED) do |x|
+      x.each do |data|
+        c = data['Country_Region'].strip
+        s = data['Province_State'].strip
+        a = data['Admin2']&.strip
+        count_up_jhu(counts, data, [c, s])
+        count_up_jhu(counts, data, [c, s, a]) if a and not a.empty?
       end
     end
+    footnote += <<_END
+Global and US data are from <a href="https://github.com/CSSEGISandData/COVID-19">CSSEGISandData/COVID-19</a> &copy; 2020 Johns Hopkins University, educational and academic research purposes only.
+_END
   end
-  footnote += <<_END
+
+  th << Thread.new do
+    $stderr.puts "Fetching and counting data for Japan for earlier data"
+    parse_data(JAPAN_CONFIRMED) do |x|
+      x.each do |data|
+        date = Time.utc(data['year'], data['month'], data['date'])
+        if date < CORONA_GO_JP_CUTOVER
+          region = ['Japan', data['prefectureNameE'].strip]
+          counts[region][date] = Integer(data['testedPositive'].gsub(/,/, ''))
+        end
+      end
+    end
+    footnote += <<_END
 Data for Japan before #{CORONA_GO_JP_CUTOVER.strftime("%Y-%m-%d")} are from <a href="https://github.com/kaz-ogiwara/covid19">kaz-ogiwara/covid19</a> &copy; TOYO KEIZAI ONLINE.
 _END
 
-  $stderr.puts "Fetching and counting data for Japan"
-  parse_data(CORONA_GO_JP, parser: JSON) do |json|
-    json['itemList'].each do |data|
-      date = parsedate(data['date'])
-      if date >= CORONA_GO_JP_CUTOVER
-        region = ['Japan', JAPAN_PREF_NAMES[data['name_jp']]]
-        counts[region][date] = Integer(data['npatients'])
+    $stderr.puts "Fetching and counting data for Japan"
+    parse_data(CORONA_GO_JP, parser: JSON) do |json|
+      json['itemList'].each do |data|
+        date = parsedate(data['date'])
+        if date >= CORONA_GO_JP_CUTOVER
+          region = ['Japan', JAPAN_PREF_NAMES[data['name_jp']]]
+          counts[region][date] = Integer(data['npatients'])
+        end
       end
     end
-  end
-  footnote += <<_END
+    footnote += <<_END
 Data for Japan are from and &copy; by <a href="https://corona.go.jp/dashboard/">Office for Novel Coronavirus Disease Control, Cabinet Secretariat, Government of Japan</a>.
 _END
 
-  $stderr.puts "Fetching and counting data for Tokyo"
-  c = Hash.new{0}
-  parse_data(TOKYO_CONFIRMED) do |x|
-    x.each do |data|
-      next unless data['公表_年月日']
-      begin
-        date = Time.strptime(data['公表_年月日'] + " UTC", "%Y-%m-%d %z")
-      rescue NoMethodError => err
-        raise err.exception(data.inspect)
+    $stderr.puts "Fetching and counting data for Tokyo"
+    c = Hash.new{0}
+    parse_data(TOKYO_CONFIRMED) do |x|
+      x.each do |data|
+        next unless data['公表_年月日']
+        begin
+          date = Time.strptime(data['公表_年月日'] + " UTC", "%Y-%m-%d %z")
+        rescue NoMethodError => err
+          raise err.exception(data.inspect)
+        end
+        c[date] += 1
       end
-      c[date] += 1
     end
-  end
-  r = ['Japan', 'Tokyo']
-  counts.delete(r)
-  n = 0
-  c.keys.sort.each do |date|
-    n += c[date]
-    counts[r][date] = n
-  end
-  footnote += <<_END
+    r = ['Japan', 'Tokyo']
+    counts.delete(r)
+    n = 0
+    c.keys.sort.each do |date|
+      n += c[date]
+      counts[r][date] = n
+    end
+    footnote += <<_END
 Data for Tokyo are from <a href="https://stopcovid19.metro.tokyo.lg.jp/">stopcovid19.metro.tokyo.lg.jp</a> &copy; 2020 Tokyo Metropolitan Government.
 _END
+  end
+
+  th.each{|t| t.join}
 
   $stderr.puts "Listing regions"
   region_tree = Array.new
